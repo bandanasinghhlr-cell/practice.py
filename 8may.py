b53d46a08f290ba3baffd4631ec71ff703582e6a
Bandana singh <bandanasingh.hlr@gmail.com>
2026-05-08 11:09:48 +0530

    msg

---
+++ 8may.py
@@ -0,0 +1,17 @@
+# loop in python
+# 1. for loop:range based
+# 2. while loop: condition based
+
+
+# 1. for loop
+# range always work with integer number.
+# range (start-0,stop-1,step:1)
+# for i in range(5,10.,1): (we can use any of the alphabate as a loop)
+    #print(i)
+    #print(i,end="") use of end for horizontal
+for i in range (1,21,-1):
+    if 1%2==0:
+        print(f"ODD:(i)")
+    else:
+        print(f"EVEN:(i)")
+# wao to takes start_point and end_point from user inpiut and print print all the number divisible by2 and 3
